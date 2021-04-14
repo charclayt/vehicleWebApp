342dca08d2d2d22384e2ce0cb74bf9108535af63
charclayt <ukfeature@gmail.com>
2021-04-14 13:24:48 +0100

    html to php

--- pages/signup.php
+++ pages/signup.php
@@ -3,6 +3,30 @@ session_start();
 
     include("connection.php");
     include("functions.php");
+
+    if($_SERVER['REQUEST_METHOD'] == "POST")
+    {
+        //something was posted
+        $user_name = $_POST['form-signup-email'];
+        $password = $_POST['form-signup-password'];
+
+        if(!empty($user_name) && !empty($password))
+        {
+            //save to database
+            $query = "insert into users (email,password) values ('$user_name','$password')";
+
+            mysqli_query($con, $query);
+
+            header("location: login.html");
+            die;
+        }
+            
+        else
+        {
+            echo "please enter valid information";
+        }
+    }
+
 ?>
 
 <!DOCTYPE html>

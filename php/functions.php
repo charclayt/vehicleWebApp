30a4518ff5b86faf4cadde62ad30e35f02a8353f
charclayt <ukfeature@gmail.com>
2021-05-09 15:15:07 +0100

    error fix
    Fixed if user hasn't selected a vehicle it displayed an error as query1 result returned a null instead of an expected array.

--- php/functions.php
+++ php/functions.php
@@ -27,11 +27,20 @@ function selected_vehicle($con, $user_email)
     $query1 = mysqli_query($con, "SELECT `vehicle_id` FROM `selected_vehicle` WHERE `user_email` = '$user_email' LIMIT 1");
     $query1_result = mysqli_fetch_assoc($query1);
 
-    $query2 = "SELECT * FROM `vehicle_info` WHERE `id` = '$query1_result[vehicle_id]' LIMIT 1";
+    if($query1_result !=null )
+    {
+        $query2 = "SELECT * FROM `vehicle_info` WHERE `id` = '$query1_result[vehicle_id]' LIMIT 1";
 
-    $result = mysqli_query($con, $query2);
+        $result = mysqli_query($con, $query2);
 
-    $final = mysqli_fetch_assoc($result);
+        $final = mysqli_fetch_assoc($result);
 
-    return $final;   
+        return $final;   
+    }
+
+    else
+    {
+        return null;
+        die;
+    }
 }
